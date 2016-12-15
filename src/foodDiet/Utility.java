5a969df317e00a6f02fc7b1254ff662053469255
xlepotato <yucia181@gmail.com>
2016-12-16 05:21:36 +0800

    bmr method

--- src/foodDiet/Utility.java
+++ src/foodDiet/Utility.java
@@ -1,11 +1,22 @@
 package foodDiet;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by Ying on 16/12/2016.
  */
 public class Utility {
 
-    public static void calculateCalories(){
+    public static double calculateBMR(double weight, double height, int age, String gender){
+
+        double bmr=0.00;
+        DecimalFormat df = new DecimalFormat("##.#");
+        if(gender.equalsIgnoreCase("male")){
+            bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        }else if(gender.equalsIgnoreCase("female")){
+            bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+        }
+        return bmr;
 
     }
 }
