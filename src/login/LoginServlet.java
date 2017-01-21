b0ed1b7c0d0757c62a9b3ae23f754737551423c8
Ian <152772a@mymail.nyp.edu.sg>
2017-01-21 20:04:45 +0800

    login page

--- src/login/LoginServlet.java
+++ src/login/LoginServlet.java
@@ -1,5 +1,8 @@
 package login;
 
+import docRecord.DoctorDAO;
+import patientRecord.PatientDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,8 +20,58 @@ public class LoginServlet extends HttpServlet {
 
         HttpSession session = request.getSession();
         String username = request.getParameter("username");
-        session.setAttribute("username",username);
-        response.sendRedirect("/fooddiet/index.jsp");
+        String password = request.getParameter("password");
+
+        PatientDAO patient = new PatientDAO();
+        DoctorDAO doctor = new DoctorDAO();
+
+
+        boolean userVerification=false;
+
+
+        if(username.isEmpty()){
+
+            response.sendRedirect("errorPage.jsp");
+
+        }else if(username.substring(0,1).equalsIgnoreCase("p")){
+
+
+            userVerification = patient.checkPatientUser(username,password);
+
+
+
+        }else if(username.substring(0,1).equalsIgnoreCase("d")){
+            userVerification = doctor.checkDoctorUser(username,password);
+
+
+        }else{
+            response.sendRedirect("errorPage.jsp");
+
+        }
+
+if(userVerification){
+
+            if(username.substring(0,1).equalsIgnoreCase("p")){
+                response.sendRedirect("patientFrontPage.jsp");
+
+            }else if(username.substring(0,1).equalsIgnoreCase("d")){
+                response.sendRedirect("docFrontPage.jsp");
+
+            }else{
+                response.sendRedirect("errorPage.jsp");
+            }
+
+}else{
+    response.sendRedirect("errorPage.jsp");
+}
+
+
+
+
+
+
+//        session.setAttribute("username",username);
+//        response.sendRedirect("/fooddiet/index.jsp");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
