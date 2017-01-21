a08dfdaa8fe86dab6e458399d175dd97a5788323
Ian <152772a@mymail.nyp.edu.sg>
2017-01-21 19:24:49 +0800

    starting on patient and doc db

--- src/patientRecord/EMF.java
+++ src/patientRecord/EMF.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 
 public class EMF {
-    private static final EntityManagerFactory emfInstance = Persistence.createEntityManagerFactory("foodOrder");
+    private static final EntityManagerFactory emfInstance = Persistence.createEntityManagerFactory("jedp");
     private EMF() {}
     public static EntityManagerFactory get() {
         return emfInstance;
