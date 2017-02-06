a203807e0507a8ef1a6db5bd43379a2dc71a6607
xlepotato <yucia181@gmail.com>
2017-02-07 02:47:24 +0800

    food api wip

--- web/fooddiet/user/foodcontent.js
+++ web/fooddiet/user/foodcontent.js
@@ -10,18 +10,68 @@ var FOOD_URL = 'https://api.nal.usda.gov/ndb/reports'
 console.log(foodResult);
 
 
+/*
 
 $.get(
     "https://api.nal.usda.gov/ndb/search/",
     {
-        "api_key": KEY1,
-        "q": foodResult
+        "q": foodResult,
+        "api_key": KEY1
+
     },
     function(data) {
         console.log(data);
         console.log( JSON.stringify(data, null, '    '))
     }
 );
+*/
+
+/*
+$.get(
+    "https://api.nal.usda.gov/ndb/search/",
+    {
+     //   "q": foodResult,
+        "q": "cheese",
+        "api_key": KEY1
+
+    },
+    function(data) {
+        console.log(data);
+        console.log( JSON.stringify(data, null, '    '))
+    }
+);
+*/
+
+$.ajax({
+    url:'https://api.nal.usda.gov/ndb/search/?q=' + foodResult  + '&api_key=' + KEY1,
+    type: 'GET',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+        console.log(data);
+        console.log( JSON.stringify(data, null, '    '))
+        $.ajax({
+            type: 'GET',
+            async: false,
+            url: 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + data.ndbno[0] + '&type=b&format=json&api_key=',
+            success: function(results) {
+                food0 = results.report.food;
+                console.log(food0);
+                $("#jsGrid-nutrition").jsGrid("insertItem", {
+                    name: food0.name,
+                    kCal: food0.nutrients[1].measures[0].value,
+                    servingSize: food0.nutrients[1].measures[0].label,
+                    quantity: food0.nutrients[1].measures[0].qty,
+                    carbs: food0.nutrients[4].value + food0.nutrients[4].unit,
+                    fats: food0.nutrients[3].value + food0.nutrients[3].unit,
+                    saturatedFats: food0.nutrients[3].value + food0.nutrients[3].unit,
+                    protein: food0.nutrients[24].value + food0.nutrients[24].unit
+                });
+    },
+    error: function (error) {
+
+    }
+});
 
 /*$.get(
     FOOD_URL,
